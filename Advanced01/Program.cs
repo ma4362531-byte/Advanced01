64ba9bde4c5bee48d0dcdb3c211de98b09221cba
ma4362531-byte <ma4362531@gmail.com>
2026-08-29 01:14:32 +0300

    Q 1

--- Advanced01/Program.cs
+++ Advanced01/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace Advanced01
+﻿using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+using System.Timers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Advanced01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Q 1
 
-            
+            //What is a generic class? Why use generics?
+
+            //    Generic Class: A class that allows code to operate on specified data types defined as parameters when instantiated.It acts as a blueprint for
+            //        type-safe data handling without fixing the data type at compile-time.
+
+            //        Reusability: Write code once and use it with any data type.
+
+            //Type Safety: Type checks occur at compile-time rather than runtime, eliminating InvalidCastException.
+
+            //Performance: Eliminates the overhead of boxing and unboxing value types (e.g., converting int to object).
+
+            #endregion
 
 
 
